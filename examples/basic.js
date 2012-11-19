3162f5e82374f5711b509a80e59981c60dda9120
AJ ONeal <coolaj86@gmail.com>
2012-11-19 13:43:17 -0700

    basic example works pretty well

--- examples/basic.js
+++ examples/basic.js
@@ -1,15 +1,24 @@
-/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true undef:true unused:true*/
 (function () {
   "use strict";
 
-  var Verimail = require('verimail')
+  var Verimail = require('../lib/verimail')
     , connect = require('connect')
     , verimail
     ;
 
   verimail = Verimail.create({
       dbfile: __dirname + '/var/verimail.json'
-    , mailer: require('./config')
+    , mailer: require('./config').mailer
+      /*
+      {
+          "service": "Gmail"
+        , "auth": {
+              "user": "john.doe@gmail.com"
+            , "pass": "super_secret"
+          }
+      }
+      */
     , callback: function (cb, id, state) {
         // Now you can retrieve by the id
         console.log(state);
