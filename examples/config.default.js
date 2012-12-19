5dd5f9663a382cfb92ae28d351312b9346edd629
AJ ONeal <coolaj86@gmail.com>
2012-12-19 14:05:55 -0700

    ignore build files and config

---
+++ examples/config.default.js
@@ -0,0 +1,14 @@
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
+(function () {
+  "use strict";
+
+  module.exports = {
+      mailer: {
+          "service": "Gmail"
+        , "auth": {
+              "user": "aj@gmail.com"
+            , "pass": "some sort of secret"
+          }
+      }
+  };
+}());
