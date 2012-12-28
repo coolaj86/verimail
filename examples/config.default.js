681c1f19d986742cb8b72e4cc2467fcdffb22106
AJ ONeal <coolaj86@gmail.com>
2012-12-28 16:54:49 -0500

    better jshint file up top

--- examples/config.default.js
+++ examples/config.default.js
@@ -1,4 +1,5 @@
-/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true
+eqeqeq:true immed:true latedef:true unused:true undef:true */
 (function () {
   "use strict";
 
